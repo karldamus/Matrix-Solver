66bd591fd399839c670d31c35097c2bd2465f7a9
Karl Damus <44273743+kraftdamus@users.noreply.github.com>
2021-02-13 13:02:01 -0500

    Add base files

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        // RowReducer rowreducer = new RowReducer();
+    }
+}
---
+++ RowReduce.java
@@ -0,0 +1,56 @@
+/**
+ * RowReduce.java
+ * Author: Karl Damus
+ * Copyright 2021, All Rights Reserved
+ */
+
+ /** The following notes are maintained by the developer of this projects. These notes are to be deprecated immediately upon release.
+  * 
+  */
+
+public class RowReduce {
+    public static int[] defaultReturnArray = new int[0]; // this is a default value --> will be removed in production
+    public static void main(String[] args) {
+
+        // todo: add user input for vectors
+
+        int[] vector1 = {1,2,3};
+        int[] vector2 = {4,5,6};
+        int[] vector3 = {2,1,0};
+        
+        int[] reducedMatrix = rowReduceMatrix(vector1, vector2, vector3);
+
+        System.out.println(reducedMatrix);
+    }
+
+    public static int[]  rowReduceMatrix(int[] vector1, int[] vector2, int[] vector3) {
+        System.out.print("Vector 1 => ");
+        for (int i : vector1) {
+            System.out.print(i);
+        }
+        System.out.println();
+
+        return defaultReturnArray;
+    }
+
+    public static int[] subtractOneVectorFromAnother(int[] array1, int[] array2) {
+
+        // todo: create a function that subtracts @param array2 from @param array1
+
+        return defaultReturnArray;
+    }
+
+    public static int[] scaleOneVector(int[] vector1, double scalar) {
+
+        // todo: create a function that returns a vector multiplied by a @param scalar value
+
+        return defaultReturnArray;
+    }
+    
+    private void printArray(int[] array) {
+        
+        // todo: create a method to print an array in a human readable format
+        // ? change function to incorporate byte[] format??
+
+    }
+}
