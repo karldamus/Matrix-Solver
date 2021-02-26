8be649c378559e15d1e39ddd67fd5ae07aea910e
karldamus <getaholdofkarl@gmail.com>
2021-02-25 21:46:25 -0500

    Create Matrix.java

---
+++ Src/Matrix.java
@@ -0,0 +1,15 @@
+package Src;
+
+public class Matrix {
+	byte[][] matrix;
+
+	public Matrix(int columns; int rows) {
+		this.matrix = new byte[columns][rows];
+	}
+
+	@Override
+	public String toString() {
+
+		return "";
+	}
+}
